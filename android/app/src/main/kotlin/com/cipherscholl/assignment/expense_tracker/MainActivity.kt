e6ba7ae5d38e9076fee885b6e93fee39eddd91fc
Shivam <suryabhardwajofficial@gmail.com>
2025-03-30 15:13:47 +0530

    initial files

---
+++ android/app/src/main/kotlin/com/cipherscholl/assignment/expense_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cipherscholl.assignment.expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff7B61FF), // Background color
+      body: Stack(
+        children: [
+          // Upper Circle (Top Right)
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/splash/upper_circle.png',
+              width: 250,
+              height: 250,
+            ),
+          ),
+          // Lower Circle (Bottom Left)
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Image.asset(
+              'assets/splash/lower_circle.png',
+              width: 250,
+              height: 250,
+            ),
+          ),
+          // for open source and community text
+          Positioned(
+            bottom: 35,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                Text(
+                  "By",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Cambay',
+                    color: Colors.white70,
+                  ),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      " Open Source ",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Cambay',
+                        color: Colors.white70,
+                      ),
+                    ),
+                    const Text(
+                      " Community",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Cambay',
+                        fontSize: 15,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // Center Logo and Text
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/splash/app_image.png',
+                  width: 150,
+                  height: 140,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "CipherX",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontFamily: 'Bruno Ace SC',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
